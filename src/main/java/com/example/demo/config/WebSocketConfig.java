45300b51bfbd265a1b889dde5485c066dd14b16c
zooey <yu.ting375@gmail.com>
2025-06-03 09:54:16 +0800

    202506030954

--- src/main/java/com/example/demo/config/WebSocketConfig.java
+++ src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -1,5 +1,11 @@
 package com.example.demo.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+
+//負責啟用 STOMP 協定並設定路由規劃
+@Configuration
+@EnableWebSocketMessageBroker // 啟用 STOMP WebScoket 支援
 public class WebSocketConfig {
 
 }
